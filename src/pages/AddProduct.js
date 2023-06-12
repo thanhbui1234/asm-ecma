d9bbefbd68fc42b78e8b81ab30d0320f3128668d
thanh <miktenthanh@gmail.com>
2023-06-12 15:12:30 +0700

    update

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -2,9 +2,21 @@ import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 import { v4 as uiidv4 } from "uuid";
 import { useEffect, router } from "../utilities";
+import Swal from "sweetalert2";
 
 const AddProduct = () => {
   useEffect(() => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
     const my_form = document.querySelector("#my-form");
     const name = document.querySelector('input[name="name"]');
     const price = document.querySelector('input[name="price"]');
@@ -15,9 +27,10 @@ const AddProduct = () => {
       const valueForm = {
         id: uiidv4(),
         name: name.value,
-        price: price.value,
+        list_price: price.value,
         description: description.value,
         short_description: short_description.value,
+        original_price: price.value,
       };
 
       fetch("http://localhost:3000/books", {
@@ -26,14 +39,17 @@ const AddProduct = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(valueForm),
+      }).then(() => {
+        navigator("/admin");
       });
     });
   });
 
   return `
   ${Nav()}
-<form id='my-form' class='tw-mt-10' action="">
   <div class='container'>
+
+<form id='my-form' class='border tw-p-10 shadow roundedyi tw-mt-10' action="">
   <div class="mb-3">
   <label for="" class="tw-text-lg form-label">Name</label>
   <input name='name' type="text" class="form-control" ">
@@ -51,9 +67,9 @@ const AddProduct = () => {
   <textarea  class="form-control" id="short_description" rows="3"></textarea>
 </div>
 <button class='addProduct btn btn-danger'>ADD</button>
-  </div>
 
 </form>
+  </div>
 
   
   ${Footer()}
